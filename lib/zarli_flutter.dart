179816e5972e698746602df7e6823c0203b8f3fe
Ed Tsoi <edtsoi430@gmail.com>
2026-02-08 21:34:17 -0800

    docs: clarify useLocalServer comment

--- lib/zarli_flutter.dart
+++ lib/zarli_flutter.dart
@@ -16,7 +16,7 @@ class ZarliFlutter {
   ///
   /// [apiKey] is optional. If not provided, the plugin attempts to read
   /// 'ZarliAPIKey' from Info.plist (iOS) or AndroidManifest.xml (Android).
-  /// [useLocalServer] - If true, points the SDK to local bidding server (debug builds only).
+  /// [useLocalServer] - If true, points the SDK to local bidding server (typically for development).
   static Future<void> initialize(
       {String? apiKey, bool useLocalServer = false}) {
     return ZarliFlutterPlatform.instance
